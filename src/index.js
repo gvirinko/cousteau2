ed10d80ea18c8930d0c210f63ba9ac347e1b6a82
Ira Gnativ <irina.gnativ@gmail.com>
2019-11-14 13:41:50 +0100

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./script.js";
-
-import "./style.css";
-
-import logoImg from './images/logo.svg';
-import closeImg from './images/close.svg';
-
-// export const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort3' : 'https://praktikum.tk/cohort3';
